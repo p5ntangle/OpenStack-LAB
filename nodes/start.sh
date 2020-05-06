02b3911638d7f5804abadc4d0bb1a5daeb7413e4
shauno <shaun@omeara.co.za>
2020-05-06 09:27:59 -0400

    odds and ends

--- nodes/start.sh
+++ nodes/start.sh
@@ -2,11 +2,11 @@
 
 
 #create shared volume for boot
-docker volume create -d flocker --name boot-files -o size=2GB
+#docker volume create -d flocker --name boot-files -o size=2GB
 
 # tftp server
-docker run -d -p 69:69/udp -v boot-files:/netboot -t som/tftpd
+docker run --name tftpd -d -p 69:69/udp -v /root/OpenStack-LAB/nodes/netboot:/netboot -t som/tftpd
 
 # DHCP Server
-docker run -d --network=host -v boot-files:/netboot -t som/dhcpd
+docker run --name dhcpd -d --network=host -v /root/OpenStack-LAB/nodes/netboot:/netboot -t som/dhcpd
 
